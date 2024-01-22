d6da93e21cde10da117ba9658d768d74c220b047
s8140156 <s8140156@gmail>
2024-01-22 14:01:38 +0800

    增加前台頁面admin,buycart,login畫面顯示,並登入路徑邏輯及驗證碼寫法

---
+++ front/buycart.php
@@ -0,0 +1,4 @@
+<?php
+if(!isset($_SESSION['mem'])){ //因為如果存在要做很多事, 所以反過來先寫 else後面再去執行落落長
+	to('?do=login');
+}
---
+++ front/login.php
@@ -0,0 +1,27 @@
+<h2>第一次購買</h2>
+<img src="./icon/0413.jpg" onclick="location.href='?do=reg'">
+
+<h2>會員登入</h2>
+<!-- table.all>tr*3>td.tt.ct+td.pp>input:text -->
+<table class="all">
+	<tr>
+		<td class="tt ct">帳號</td>
+		<td class="pp"><input type="text" name="acc" id="acc"></td>
+	</tr>
+	<tr>
+		<td class="tt ct">密碼</td>
+		<td class="pp"><input type="text" name="pw" id="pw"></td>
+	</tr>
+	<tr>
+		<td class="tt ct">驗證碼</td>
+		<td class="pp">
+			<?php
+			$a=rand(10,99);
+			$b=rand(10,99);
+			$_SESSION['ans']=$a+$b; //存session at server client端看不到
+			echo $a . "+" .$b . " =" //注意字串寫法 可以讓=排版好看
+			?>
+			<input type="text" name="chk" id="chk"></td>
+	</tr>
+</table>
+<div class="ct"><button>確認</button></div>
