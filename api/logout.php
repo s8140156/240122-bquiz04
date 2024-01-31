cf05c5834fb0dead2869fbe3fdd076dd8ef24d4c
s8140156 <s8140156@gmail>
2024-01-31 08:56:43 +0800

    會員及管理員登入狀態管理 及登出機制及建構管理登入功能畫面

---
+++ api/logout.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+unset($_SESSION['mem'],$_SESSION['admin']);
+header('location:../index.php');
+
+
+//一般來說,在外網頁 登出 一個多重身分(ex.客戶+管理員...)還有確認機制
+//目前這樣寫法 當會員與管理員都登入時，不管誰logout會全部登出
+?>
+
