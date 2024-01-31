31863e720a25fdd377b6a7c3a8c1bd19bc39307a
s8140156 <s8140156@gmail>
2024-01-31 11:35:34 +0800

    增商品分類及管理畫面呈現

---
+++ back/th.php
@@ -0,0 +1,47 @@
+<h2 class="ct">商品分類</h2>
+<div class="ct">
+	新增大分類 <input type="text" name="big" id="big"><button>新增</button></div>
+<div class="ct">
+	新增中分類
+	<select name="big" id="bigs"></select>
+	<input type="text" name="mid" id="mid"><button>新增</button>
+</div>
+<table class="all">
+	<tr class="tt">
+		<td>流行皮件</td>
+		<td class="ct">
+			<button>修改</button>
+			<button>刪除</button>
+		</td>
+	</tr>
+	<tr class="pp ct">
+		<td>女用皮件</td>
+		<td>
+			<button>修改</button>
+			<button>刪除</button>
+		</td>
+	</tr>
+</table>
+<h2 class="ct">商品管理</h2>
+<div class="ct"><button>新增商品</button></div>
+<table class="all">
+	<tr class="tt ct">
+		<th>編號</th>
+		<th>商品名稱</th>
+		<th>庫存量</th>
+		<th>狀態</th>
+		<th>操作</th>
+	</tr>
+	<tr class="pp">
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td>
+			<button>修改</button>
+			<button>刪除</button>
+			<button>上架</button>
+			<button>下架</button>
+		</td>
+	</tr>
+</table>
